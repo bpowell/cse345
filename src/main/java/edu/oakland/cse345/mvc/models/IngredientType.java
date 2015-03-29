1e57394ccbb09019c969daa2cf41c820a6124fa5
Brandon Powell <bpowell29a@gmail.com>
2015-03-29 16:49:16 -0400

    Added model for IngredientType

---
+++ src/main/java/edu/oakland/cse345/mvc/models/IngredientType.java
@@ -0,0 +1,13 @@
+package edu.oakland.cse345.mvc.models;
+
+public class IngredientType {
+        public int id;
+        public String name;
+
+        IngredientType(int id, String name) {
+                this.id = id;
+                this.name = name;
+        }
+
+        IngredientType() {}
+}
